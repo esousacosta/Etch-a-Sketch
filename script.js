8dbb77097de8799f76f9e46933a2da07a9599a37
esousacosta <victoremanoel23@gmail.com>
2022-05-15 15:53:27 +0200

    Finish basic functionality of painting at hover event

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict"
 
 var SKETCH_PAD_SIZE_IN_PX = 640;
-const aSubmitPadSizeInputButton = document.getElementById("submitPadSizeInput");
 
 function removeAllSketchPadElements ()
 {
@@ -25,23 +24,56 @@ function resizeChildrenDivs (ioSketchPadDiv, iSketchPadNumOfElementsByRow)
     });
 }
 
-function updateSketchPadDimensions ()
+/**
+ * 
+ * @param {object} oMouseoverPadEvent a single pad element hovered over(i.e.: a square)
+ */
+function paintElementBlack (oMouseoverPadEvent)
+{
+    console.log("Entrei e pintei");
+    oMouseoverPadEvent.target.classList.add("blackPainted");
+}
+
+/**
+ * 
+ * @param {object} oSketchPadElementsList List of all children nodes
+ */
+function addPaintEffectOnHovering (oSketchPadElementsList)
+{
+    console.log(oSketchPadElementsList);
+    console.log("entrie");
+    oSketchPadElementsList.forEach(aSketchPadElement => 
+    {
+        aSketchPadElement.addEventListener('mouseover', paintElementBlack);
+    });
+}
+
+function updateSketchPad()
 {
     const aSketchPadSizeInputField = document.getElementById("padSizeInput");
     const aSketchPadDiv = document.getElementById("sketchPad");
-    let aSketchPadNumOfElementsByRow = 16;
+    let aSketchPadNumOfElementsByRow;
     
     removeAllSketchPadElements();
 
     aSketchPadNumOfElementsByRow = Number(aSketchPadSizeInputField.value);
-    const aTotalNumberOfElements = aSketchPadNumOfElementsByRow ** 2;
-    for (let i = 0; i < aTotalNumberOfElements; i++)
+    if (aSketchPadNumOfElementsByRow > 100 || aSketchPadNumOfElementsByRow < 10)
     {
-        const aNewChildDivElement = document.createElement('div');
-        aNewChildDivElement.classList.add('sketchPadElement');
-        aSketchPadDiv.appendChild(aNewChildDivElement);
+        alert("Invalid amount of squares in the pad. Try again!");
     }
-    resizeChildrenDivs(aSketchPadDiv, aSketchPadNumOfElementsByRow);
+    else
+    {
+        const aTotalNumberOfElements = aSketchPadNumOfElementsByRow ** 2;
+        for (let i = 0; i < aTotalNumberOfElements; i++)
+        {
+            const aNewChildDivElement = document.createElement('div');
+            aNewChildDivElement.classList.add('sketchPadElement');
+            aSketchPadDiv.appendChild(aNewChildDivElement);
+        }
+        resizeChildrenDivs(aSketchPadDiv, aSketchPadNumOfElementsByRow);
+        addPaintEffectOnHovering(aSketchPadDiv.childNodes);
+    }
 }
 
-aSubmitPadSizeInputButton.addEventListener('click', updateSketchPadDimensions);
+const aSubmitPadSizeInputButton = document.getElementById("submitPadSizeInput");
+aSubmitPadSizeInputButton.addEventListener('click', updateSketchPad);
