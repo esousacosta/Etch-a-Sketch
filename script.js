bb8cefd1bcb5b46e15a75a1e1ac7b0bd390df92e
esousacosta <victoremanoel23@gmail.com>
2022-05-15 12:49:14 +0200

    Fix the grid spacing horizontaly; Automatically sizing the children

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict"
 
+var SKETCH_PAD_SIZE_IN_PX = 640;
 const aSubmitPadSizeInputButton = document.getElementById("submitPadSizeInput");
 
 function removeAllSketchPadElements ()
@@ -11,21 +12,36 @@ function removeAllSketchPadElements ()
     }
 }
 
+/**
+ * 
+ * @param {Object} ioSketchPadDiv the main div container
+ */
+function resizeChildrenDivs (ioSketchPadDiv, iSketchPadNumOfElementsByRow)
+{
+    const aChildrenDivList = ioSketchPadDiv.childNodes;
+    aChildrenDivList.forEach(aChildDiv => {
+        aChildDiv.style.width = Math.floor(SKETCH_PAD_SIZE_IN_PX / iSketchPadNumOfElementsByRow) + 'px';
+        aChildDiv.style.height = Math.floor(SKETCH_PAD_SIZE_IN_PX / iSketchPadNumOfElementsByRow) + 'px';
+    });
+}
+
 function updateSketchPadDimensions ()
 {
     const aSketchPadSizeInputField = document.getElementById("padSizeInput");
     const aSketchPadDiv = document.getElementById("sketchPad");
-    let aSketchPadSize = 16;
+    let aSketchPadNumOfElementsByRow = 16;
     
     removeAllSketchPadElements();
 
-    aSketchPadSize = Number(aSketchPadSizeInputField.value);
-    for (let i = 0; i < aSketchPadSize; i++)
+    aSketchPadNumOfElementsByRow = Number(aSketchPadSizeInputField.value);
+    const aTotalNumberOfElements = aSketchPadNumOfElementsByRow ** 2;
+    for (let i = 0; i < aTotalNumberOfElements; i++)
     {
         const aNewChildDivElement = document.createElement('div');
-        aNewChildDivElement.textContent = i;
+        aNewChildDivElement.classList.add('sketchPadElement');
         aSketchPadDiv.appendChild(aNewChildDivElement);
     }
+    resizeChildrenDivs(aSketchPadDiv, aSketchPadNumOfElementsByRow);
 }
 
 aSubmitPadSizeInputButton.addEventListener('click', updateSketchPadDimensions);
